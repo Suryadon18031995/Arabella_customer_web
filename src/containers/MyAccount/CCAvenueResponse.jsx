18615b9c7f18054d663c6164d5df329f4abcbf32
aneesh upadhyaya <aneeshupadhyayach@gmail.com>
2019-11-06 17:34:40 +0530

    cc avenue updation

--- src/containers/MyAccount/CCAvenueResponse.jsx
+++ src/containers/MyAccount/CCAvenueResponse.jsx
@@ -1,136 +1,122 @@
 import React, { Component } from 'react';
 import _get from 'lodash/get';
-import _find from 'lodash/find';
-import _isEmpty from 'lodash/isEmpty';
 import _isError from 'lodash/isError';
-import _filter from 'lodash/filter';
-// import CryptoJS from 'crypto-js';
-// import axios from 'axios';
+import _isEmpty from 'lodash/isEmpty';
 import Redirect from 'react-router/Redirect';
 import connect from 'react-redux/lib/connect/connect';
-import { fetchAllAddressData, setAddrId } from '../../actions/address';
-import { fetchFirstCartData } from '../../actions/cart';
-import { SubscriptionLoader } from '../../components/Loader/Loader.jsx';
-import {
-    // getSubscriptionHelperDetails,
-    fetchPlaceOrderData,
-    clearPlaceOrderReducer,
-    fetchPaymentMethodInfo,
-    addFirstDataCreditCard,
-    addPaypalCreditCard,
-    getSavedCardDetails,
-    getOrderId,
-    getBraintreeClientToken,
-} from '../../actions/placeOrder';
-import { upgradePrimeMembershipData } from '../../actions/myOrder';
-// import { handleValidation } from '../../helpers/checkoutValidation.jsx';
-// import { mapPaypalData, mapfirstData, mapAuthorizeNetData, mapBraintreeData, mapCCAvenueData } from '../../utils/commonMapper';
-import BreadCrumbs from '../../components/Common/BreadCrumbs.jsx';
-import ErrorBoundary from '../ErrorBoundary.jsx';
+import '../../assets/stylesheets/checkout.css';
+import { fetchPlaceOrderData, clearPlaceOrderReducer, updateProductQty } from '../../actions/placeOrder';
 import ErrorHandler from '../../components/Hoc/ErrorHandler.jsx';
+import { flushCartViewData } from '../../actions/cart';
+import { flushCartData, updatePrimeValue } from '../../actions/login';
+
+// const reduceObject = (data) => {
+//     const abc = data && data.map(each => ({
+//         name: each.name,
+//         price: each.product_price,
+//         quantity: each.qty,
+//         sku: each.sku,
+//     }));
+//     return abc;
+// };
+
+const queryStringToJSON = (data) => {
+    const pairs = data.split('&');
+    const result = {};
+    pairs.forEach((pair) => {
+        pair = pair.split('=');
+        result[pair[0]] = decodeURIComponent(pair[1] || '');
+    });
+    return JSON.parse(JSON.stringify(result));
+};
 
 class CCAvenueResponseHandler extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            showHome: false,
             paymentRes: {},
-            amount: 0,
-            rewardPointsUsed: 0,
-            rewardsChecked: false,
-            minReward: 0,
-            maxReward: 100,
-            earnPoints: 0,
-            pointBalance: 0,
-            enableSpendPoint: false,
-            breadCrumbsList: [
-                {
-                    link: '/',
-                    name: 'home',
-                },
-                {
-                    link: undefined,
-                    name: 'CHECKOUT',
-                },
-            ],
-            payMethod: undefined,
-            cartType: _get(this.props.cartType, 'cartType'),
-            addrId: this.props.addrId,
-            paymentType: 'firstdataglobalgateway',
-            showCheckoutSuccess: false,
-            errors: {},
-            checked: true,
-            country: 'US',
-            expandIndex: 1,
-            showCredit: false,
-            expirymonth: undefined,
-            expiryyear: undefined,
-            ccNum: undefined,
-            thisYear: (new Date()).getFullYear(),
-            tokenize: false,
-            // transactionType: _get(this.props.cartType, 'cartType'),
-            transactionType: undefined,
-            showShipAddress: true,
-            showCards: true,
-            currencyCode: 'USD',
-            cartId: this.props.cartId,
-            transactions: [
-                {
-                    amount: {
-                        total: this.checkoutTotal,
-                        currency: 'USD',
-                    },
-                    invoice_number: this.reservedOrderId,
-                },
-            ],
-            showRadio: undefined,
-            showNewCard: undefined,
-            productionIds: '',
-            cycles: '',
-            couponCode: '',
-            discount: '',
-            feeAmount: '',
-            placeOrderAmount: 0,
-            savedCardsFirstdata: [],
-            savedCardsAuthorizenet: [],
         };
     }
 
+    handleContinueClick = () => {
+        this.setState({ showHome: true });
+    }
+
     componentDidMount() {
-        document.title = 'Checkout Success';
+        // const script = document.createElement('script');
+        // // script.src = 'https://www.googletagmanager.com/gtag/js?id=AW-923729795';
+        // script.text = `gtag('event', 'conversion', {
+        //     send_to: 'AW-923729795/mR6KCLjaooEBEIP_u7gD',
+        //     value: $${_get(this.props.history, 'location.state.placeOrderAmount')},
+        //     currency: 'USD',
+        //     transaction_id: ${_get(this.props.history, 'location.state.orderId')},
+        // });`;
+        // script.type = 'text/javascript';
 
-        const abc = "?id=100025283&amount=99.99&status=Success";
+        // const purchaseScript = document.createElement('script');
+        // purchaseScript.text = `window.dataLayer = window.dataLayer || [];
+        // window.dataLayer.push({
+        //   event: 'purchase',
+        //   ecommerce: {
+        //     purchase: {
+        //       actionField: {
+        //         id: ${_get(this.props.history, 'location.state.orderId')}
+        //         revenue: $${_get(this.props.history, 'location.state.placeOrderAmount')},
+        //         tax: '0.00',
+        //         shipping: '0.00'
+        //       },
+        //     }
+        //   }
+        // });
+        // </script>`;
+        // purchaseScript.type = 'text/javascript';
+        // document.body.appendChild(script);
+        // document.body.appendChild(purchaseScript);
 
-        console.log(this.QueryStringToJSON(abc.slice(1)));
+        // const pdData = JSON.stringify(reduceObject(this.props.productInfo));
+
+        // new script for ecom track
+
+        // const purchaseScriptNew = document.createElement('script');
+        // purchaseScriptNew.text = `window.dataLayer = window.dataLayer || [];
+        // window.dataLayer.push({
+        //   'event': 'ecomm_event',
+        //   'transactionId': ${_get(this.props.history, 'location.state.orderId')},
+        //     'transactionTotal': ${_get(this.props.history, 'location.state.placeOrderAmount')},
+        //     'transactionTax': 0.00 ,
+        //     'transactionShipping': 0.00,
+        //     'transactionProducts': ${pdData}
+        // });`;
+        // purchaseScriptNew.type = 'text/javascript';
+        // document.body.appendChild(purchaseScriptNew);
+        const abc = _get(this.props, 'location.search');
+
+        // console.log(this.queryStringToJSON(abc.slice(1)));
+        const paymentRes = queryStringToJSON(abc.slice(1));
         this.setState({
-            paymentRes: this.QueryStringToJSON(abc.slice(1)),
+            paymentRes,
         });
+
+        if (_get(paymentRes, 'status') === 'Success') {
+            this.props.flushCartViewData();
+            this.props.flushCartData();
+            this.props.updateProductQty({ productIds: _get(this.props.history, 'location.state.productIds') });
+            if (this.props.cartType === 'prime') {
+                this.props.updatePrimeValue('1');
+            }
+        }
     }
 
-    QueryStringToJSON = (data) => {            
-        let pairs = data.split('&');
-        
-        let result = {};
-        pairs.forEach((pair) => {
-            pair = pair.split('=');
-            result[pair[0]] = decodeURIComponent(pair[1] || '');
-        });
-    
-        return JSON.parse(JSON.stringify(result));
-    }
-    
     render() {
-        // console.log(this.props.braintreeClientToken);
-        if (_get(this, 'props.isLoading')) {
-            return (
-                <div className="container" style={{ minHeight: '500px' }}>
-                    <SubscriptionLoader type={_get(this.props, 'cartType', '')} actionType={_get(this.props, 'actionType', '')} />
-                </div>
-            );
+        if (_get(this.state, 'showHome')) {
+            return <Redirect push to="/" />;
         }
-        if (_get(this.state, 'showCheckoutSuccess')) {
+
+        if (_get(this.state, 'showViewOrder')) {
             return <Redirect push to={{
-                pathname: '/checkout/onepage/success',
-                state: { orderId: this.state.orderId, productIds: this.state.productIds, placeOrderAmount: this.state.placeOrderAmount },
+                pathname: '/customer/account/viewOrder',
+                state: { orderId: this.state.orderId },
             }} />;
         }
         if (!this.props.apiToken) {
@@ -138,107 +124,75 @@ class CCAvenueResponseHandler extends Component {
                 pathname: '/login',
             }} />;
         }
-        console.log('props:', this.props);
+
         return (
-            <div>
-                <BreadCrumbs
-                    list={this.state.breadCrumbsList} />
-                <div className="container">
-                    <div ref={this.abc} />
-                    <ErrorBoundary>
-                    Hey!
-                    {this.props.location.pathname.toLowerCase() === '/ccavenuecancelhandler' ? <div>You cancelled your payment</div> : ''} 
-                    {this.state.paymentRes ? <div>
-                        <div>Order Id: {_get(this.state.paymentRes, 'id')}</div>
-                        <div>Order Status: {_get(this.state.paymentRes, 'status')}</div>
-                        <div>Amount: {_get(this.state.paymentRes, 'amount')}</div>
-                    </div> : null}
-                    </ErrorBoundary>
+            <div className="container">
+                    {this.props.location.pathname.toLowerCase() === '/ccavenuecancelhandler' ? <div>Hey! You cancelled your payment.</div> : ''}
+                    {this.state.paymentRes && this.props.location.pathname.toLowerCase() !== '/ccavenuecancelhandler' ? <div className="checkout-success">
+                    <div className="page-title">
+                        <h1>Your order has been received.</h1>
+                    </div>
+                    <div className="success-block">
+                        <h2 className="sub-title">Thank you for your purchase!</h2>
+                        {/* <div>Order Status: {_get(this.state.paymentRes, 'status')}</div> */}
+                        <p>Your order # is: {_get(this.state.paymentRes, 'id')}. Your payment of INR {_get(this.state.paymentRes, 'amount')} was successfully processed. </p>
+                        <p>You will receive an order confirmation email with details of your order and a link to track its progress.</p>
+                        <div className="buttons-set">
+                            <button type="button" className="button" title="Continue Shopping" onClick={this.handleContinueClick}><span><span>Continue Shopping</span></span></button>
+                        </div>
+                    </div>
                 </div>
+                : <div>Payment Failed.</div>}
             </div>
         );
     }
 }
 
 const mapDispatchToProps = dispatch => ({
-    getAllAddressData: data => dispatch(fetchAllAddressData(data)),
-    getCartData: data => dispatch(fetchFirstCartData(data)),
     getPlaceOrder: data => dispatch(fetchPlaceOrderData(data)),
     clearPlaceOrderData: () => { dispatch(clearPlaceOrderReducer()); },
-    getPaymentMethodInfo: data => dispatch(fetchPaymentMethodInfo(data)),
-    setAddrId: data => dispatch(setAddrId(data)),
-    // getSubscriptionHelp: data => dispatch(getSubscriptionHelperDetails(data)),
-    addFirstDataCreditCard: (data, url) => dispatch(addFirstDataCreditCard(data, url)),
-    addPaypalCreditCard: data => dispatch(addPaypalCreditCard(data)),
-    getSavedCardData: data => dispatch(getSavedCardDetails(data)),
-    getOrderId: data => dispatch(getOrderId(data)),
-    upgradePrimeMembershipData: data => dispatch(upgradePrimeMembershipData(data)),
-    getBraintreeClientToken: () => dispatch(getBraintreeClientToken()),
+    flushCartViewData: () => dispatch(flushCartViewData()),
+    flushCartData: () => dispatch(flushCartData()),
+    updateProductQty: data => dispatch(updateProductQty(data)),
+    updatePrimeValue: data => dispatch(updatePrimeValue(data)),
 });
 
 const mapStateToProps = (state) => {
     const {
-        loginReducer, allAddressReducer, cartReducer, placeOrderReducer,
+        loginReducer, placeOrderReducer, cartReducer,
     } = state;
 
     const {
         apiToken,
         currencyCode,
-        storeId,
-        error: loginError,
         cartId,
-        primeUser,
-        demoExpired,
+        error: loginError,
     } = loginReducer || [];
 
     const {
-        allAddressData,
-        addrId,
-        error: addressError,
-    } = allAddressReducer || [];
-
-    const {
-        firstCartData,
-        cartType,
-        error: cartError,
-    } = cartReducer || [];
-
-    const {
         placeOrderData,
-        paymentMethodInfoData,
-        paypalCreditCardRes,
-        firstDataCreditCardRes,
-        isFetching: isLoading,
-        savedCardResult,
-        orderIdData,
+        firstData,
         error: placeOrderError,
-        type: actionType,
-        braintreeClientToken,
     } = placeOrderReducer || [];
 
-    const error = !_isEmpty(placeOrderError) || _isError(placeOrderError) || !_isEmpty(cartError) || _isError(cartError) || !_isEmpty(addressError) || _isError(addressError) || !_isEmpty(loginError) || _isError(loginError);
+    const {
+        cartType,
+        productInfo,
+    } = cartReducer || [];
+
+    const error = !_isEmpty(placeOrderError) || _isError(placeOrderError) || !_isEmpty(loginError) || _isError(loginError);
+
     return {
         apiToken,
         currencyCode,
-        allAddressData,
-        storeId,
-        firstCartData,
-        placeOrderData,
-        paymentMethodInfoData,
-        isLoading,
-        cartType,
-        addrId,
-        paypalCreditCardRes,
-        firstDataCreditCardRes,
-        savedCardResult,
-        orderIdData,
-        error,
         cartId,
-        primeUser,
-        demoExpired,
-        actionType,
-        braintreeClientToken,
+        placeOrderData,
+        firstData,
+        error,
+        cartType,
+        productInfo,
     };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ErrorHandler(CCAvenueResponseHandler));
+
